84b610a2c6187d2fdc6cfcbeddc7d1d3ddeeb335
Tuckson <franrache@gmail.com>
2022-10-21 14:36:29 -0300

    intento numero 12032748973
    tener piedad pls jaja

--- src/franco/Ejercicio1/Ejercicio1.java
+++ src/franco/Ejercicio1/Ejercicio1.java
@@ -4,55 +4,69 @@ import java.util.Scanner;
 public class Ejercicio1 {
     public static void main(String args[]) {
 
-        int numeroDeAfiliado = 0;
-        ArrayList<Integer> pacientesDeUrgencia = new ArrayList<>();
-        ArrayList<Integer> pacientesConTurno = new ArrayList<>();
 
-        while (numeroDeAfiliado!=-1) {
-            ArrayList<Integer> cantidadDeAfiliados = new ArrayList<>();
-            Scanner scannerDeSocios = new Scanner(System.in);
-
-
-            System.out.println("Digite su numero de afiliado: ");
-            numeroDeAfiliado = scannerDeSocios.nextInt();
-            cantidadDeAfiliados.add(numeroDeAfiliado);
-
-
-            ArrayList<Integer> cantidadDeTurnos = new ArrayList<>();
-            Scanner scannerDeTurnos = new Scanner(System.in);
-            int tipoDeAtencion;
-
-            System.out.println("Digite 0 si es urgencia o 1 si es por turno: ");
-            tipoDeAtencion = scannerDeTurnos.nextInt();
-            cantidadDeTurnos.add(tipoDeAtencion);
-
-            boolean estaRepetido = false;
-            int contadorTurno = 1;
-            int contadorUrgencia = 1;
-
-            if(tipoDeAtencion==0) {
-                if (estaRepetido = false) {
-                    pacientesDeUrgencia.add(numeroDeAfiliado);
-                } else {
-                    estaRepetido=true;
-                    contadorUrgencia++;
-                }
-
-            }
-            if(tipoDeAtencion==1){
-                if(estaRepetido=false){
-                    pacientesConTurno.add(numeroDeAfiliado);
-                } else {
-                    estaRepetido=true;
-                    contadorTurno++;
-                }
-            }
-
-        }
-        System.out.println("Los pacientes atendidos por urgencia son " + pacientesDeUrgencia);
-        System.out.println("Los pacientes atendidos por turno son " + pacientesConTurno);
-
+consultorio(5555);
 
     }
+   public static void consultorio (int numeroDeAfiliado) {
 
+       ArrayList<Integer> pacientesDeUrgencia = new ArrayList<>();
+       ArrayList<Integer> pacientesConTurno = new ArrayList<>();
+
+       while (numeroDeAfiliado != -1) {
+
+           Scanner scannerDeSocios = new Scanner(System.in);
+
+
+           System.out.println("Digite su numero de afiliado: ");
+           numeroDeAfiliado = scannerDeSocios.nextInt();
+
+
+           Scanner scannerDeTurnos = new Scanner(System.in);
+           int tipoDeAtencion;
+
+           System.out.println("Digite 0 si es urgencia o 1 si es por turno: ");
+           tipoDeAtencion = scannerDeTurnos.nextInt();
+
+
+           if (tipoDeAtencion == 0) {
+               pacientesDeUrgencia.add(numeroDeAfiliado);
+
+           }
+           if (tipoDeAtencion == 1) {
+               pacientesConTurno.add(numeroDeAfiliado);
+           }
+
+       }
+       System.out.println("Los pacientes atendidos por urgencia son " + pacientesDeUrgencia);
+       System.out.println("Los pacientes atendidos por turno son " + pacientesConTurno);
+
+       int cantidadDeUrgencias = 0;
+       int cantidadDeTurnos = 0;
+       for (int i = 0; i < pacientesDeUrgencia.size(); i++) {
+
+           if (numeroDeAfiliado == pacientesDeUrgencia.get(i)) {
+               cantidadDeUrgencias++;
+           }
+
+       }
+
+       for (int i = 0; i < pacientesConTurno.size(); i++) {
+
+           if (numeroDeAfiliado == pacientesConTurno.get(i)) {
+               cantidadDeTurnos++;
+           }
+
+
+
+       }
+       System.out.println("El afiliado se atendio " + cantidadDeUrgencias + " veces de urgencia");
+       System.out.println("El afiliado se atencio " + cantidadDeTurnos + " veces con turno");
+
+   }
 }
+
+
+
+
+
